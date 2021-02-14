ea9903ddddb191171c106d84943e84f715d2d5fe
Ernestas <ernestas.okunevic@gmail.com>
2021-02-14 04:30:45 +0200

    Relocating code from MainActivity to MainFragment

--- app/src/main/java/com/ernokun/noteapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ernokun/noteapp/ui/activities/MainActivity.kt
@@ -1,16 +1,12 @@
-package com.ernokun.noteapp
+package com.ernokun.noteapp.ui.activities
 
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.RecyclerView
+import com.ernokun.noteapp.R
 import com.ernokun.noteapp.room.applications.NotesApplication
 import com.ernokun.noteapp.room.entities.Note
 import com.ernokun.noteapp.room.viewModels.NoteViewModel
@@ -19,7 +15,6 @@ import com.ernokun.noteapp.ui.dialogs.AddNoteDialog
 import com.ernokun.noteapp.ui.dialogs.UpdateNoteDialog
 import com.ernokun.noteapp.ui.fragments.MainFragment
 import com.ernokun.noteapp.utils.NoteAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), AddNoteDialog.AddNoteDialogListener,
     NoteAdapter.OnPressedNoteItem, UpdateNoteDialog.UpdateNoteDialogListener {
@@ -29,7 +24,6 @@ class MainActivity : AppCompatActivity(), AddNoteDialog.AddNoteDialogListener,
         NoteViewModelFactory((application as NotesApplication).repository)
     }
 
-//    private lateinit var fragment_container_view_fragmentContainer: FragmentContainerView
     private lateinit var mainFragment: Fragment
 
 
@@ -37,55 +31,13 @@ class MainActivity : AppCompatActivity(), AddNoteDialog.AddNoteDialogListener,
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        findViews()
-
         mainFragment = MainFragment(noteViewModel)
 
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             add(R.id.fragment_container_view_fragmentContainer, mainFragment)
         }
-
-
-//        prepareNoteAdapter()
-//
-//        noteViewModel.allNotes.observe(this, Observer { notes ->
-//            notes?.let {
-//                noteList = it
-//                noteAdapter.submitList(noteList)
-//            }
-//        })
-//
-//        addNoteButton.setOnClickListener {
-//            showAddNoteDialog()
-//        }
-//
-//        editText_searchBar.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
-//            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-//                //Perform Code
-//                val filterString: String = editText_searchBar.text.toString()
-//                filterList(filterString)
-//
-//                return@OnKeyListener true
-//            }
-//            false
-//        })
     }
-
-
-//    private fun findViews() {
-////        editText_searchBar = findViewById(R.id.editText_searchBar)
-////        addNoteButton = findViewById(R.id.floatingActionButton_addNote)
-////        recyclerView_notes = findViewById(R.id.recyclerView_notes)
-//
-////        fragment_container_view_fragmentContainer = findViewById(R.id.fragment_container_view_fragmentContainer)
-//    }
-//
-//
-//    private fun prepareNoteAdapter() {
-//        noteAdapter = NoteAdapter(this)
-//        recyclerView_notes.adapter = noteAdapter
-//    }
 
 
     /**
@@ -114,6 +66,9 @@ class MainActivity : AppCompatActivity(), AddNoteDialog.AddNoteDialogListener,
     }
 
 
+    /**
+     *  Implemented in the NoteAdapter
+     */
     override fun deleteNote(note: Note) {
         noteViewModel.delete(note)
         showToast("Note was deleted!")
@@ -127,14 +82,4 @@ class MainActivity : AppCompatActivity(), AddNoteDialog.AddNoteDialogListener,
         noteViewModel.update(note)
     }
 
-
-
-//    private fun filterList(noteFilter: String) {
-//        val filteredNoteList: List<Note> =
-//            noteList.filter { note ->
-//                note.noteText.toLowerCase().contains(noteFilter.toLowerCase())
-//            }
-//
-//        noteAdapter.submitList(filteredNoteList);
-//    }
 }
--- app/src/main/java/com/ernokun/noteapp/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/ernokun/noteapp/ui/fragments/MainFragment.kt
@@ -1,15 +1,20 @@
 package com.ernokun.noteapp.ui.fragments
 
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.ernokun.noteapp.R
 import com.ernokun.noteapp.room.applications.NotesApplication
 import com.ernokun.noteapp.room.entities.Note
 import com.ernokun.noteapp.room.viewModels.NoteViewModel
 import com.ernokun.noteapp.room.viewModels.NoteViewModelFactory
+import com.ernokun.noteapp.ui.dialogs.AddNoteDialog
 import com.ernokun.noteapp.utils.NoteAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -17,11 +22,74 @@ class MainFragment(val noteViewModel: NoteViewModel) : Fragment(R.layout.fragmen
 
     private lateinit var noteList: List<Note>
 
-
     private lateinit var editText_searchBar: EditText
     private lateinit var addNoteButton: FloatingActionButton
     private lateinit var recyclerView_notes: RecyclerView
 
     private lateinit var noteAdapter: NoteAdapter
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        findViews(view)
+
+        prepareNoteAdapter()
+
+        observeNoteData()
+
+        initAddButtonListener()
+        initSearchBarListener()
+    }
+
+    private fun observeNoteData() {
+        noteViewModel.allNotes.observe(viewLifecycleOwner, Observer { notes ->
+            notes?.let {
+                noteList = it
+                noteAdapter.submitList(noteList)
+            }
+        })
+    }
+
+    private fun initAddButtonListener() {
+        addNoteButton.setOnClickListener {
+            /**
+             *  TODO make an interface for this fragment and implement it in the MainActivity
+             *  it should change the fragment to a new one
+             */
+        }
+    }
+
+    private fun initSearchBarListener() {
+        editText_searchBar.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                //Perform Code
+                val filterString: String = editText_searchBar.text.toString()
+                filterList(filterString)
+
+                return@OnKeyListener true
+            }
+            false
+        })
+    }
+
+    private fun findViews(view: View) {
+        editText_searchBar = view.findViewById(R.id.editText_searchBar)
+        addNoteButton = view.findViewById(R.id.floatingActionButton_addNote)
+        recyclerView_notes = view.findViewById(R.id.recyclerView_notes)
+    }
+
+    private fun prepareNoteAdapter() {
+        noteAdapter = context?.let { NoteAdapter(it) }!!
+        recyclerView_notes.adapter = noteAdapter
+    }
+
+    private fun filterList(noteFilter: String) {
+        val filteredNoteList: List<Note> =
+            noteList.filter { note ->
+                note.noteText.toLowerCase().contains(noteFilter.toLowerCase())
+            }
+
+        noteAdapter.submitList(filteredNoteList);
+    }
 }
